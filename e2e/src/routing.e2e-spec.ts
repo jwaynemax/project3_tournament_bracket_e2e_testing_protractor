75bcb370cd188b958a8ef5413c6d647f5ae306e5
jm00724 <jm00724@cs6261-vm12.uwg.westga.edu>
2023-06-29 20:53:47 -0400

    test Welcome Page h2 equals Brackets App

--- e2e/src/routing.e2e-spec.ts
+++ e2e/src/routing.e2e-spec.ts
@@ -1,12 +1,13 @@
-import { browser, logging } from 'protractor';
+import { browser, by, element } from 'protractor';
 
 describe('Welcome page', () => {
 
-  it('should check that h2 element is brackets app', () => {
+  it('should check that h2 element equals Brackets App', () => {
     browser.get('/');
 
-    var h2 = element(by.binding('title'));
-    expect(h2.getText()).toBe('Brackets App');
+    var title = element(by.id('subpageTitle'));
+
+    expect(title.getText()).toEqual('Brackets App');
 
   });
 });
