7e82b4c6414d5580a2bb1e643c4bead1ab868350
jm00724 <jm00724@cs6261-vm12.uwg.westga.edu>
2023-06-29 21:11:34 -0400

    tests added to test navigtating to registration, brackets, and back to home.

--- e2e/src/routing.e2e-spec.ts
+++ e2e/src/routing.e2e-spec.ts
@@ -11,3 +11,42 @@ describe('Welcome page', () => {
 
   });
 });
+
+describe('Registration page', () => {
+
+  it('should check that h2 element equals Register Players', () => {
+    browser.get('/');
+
+    var registration = element(by.id('registration')).click();
+    var title = element(by.id('registration-title'));
+
+    expect(title.getText()).toEqual('Register Players');
+
+  });
+});
+
+describe('Brackets page', () => {
+
+  it('should check that h2 element equals Brackets', () => {
+    browser.get('/');
+
+    var registration = element(by.id('brackets')).click();
+    var title = element(by.id('subpageTitle'));
+
+    expect(title.getText()).toEqual('Brackets');
+
+  });
+});
+
+describe('User', () => {
+
+  it('should be able to navigate back to the Welcome page from /brackets', () => {
+    browser.get('/brackets');
+
+    var registration = element(by.id('hello')).click();
+    var title = element(by.id('subpageTitle'));
+
+    expect(title.getText()).toEqual('Brackets App');
+
+  });
+});
